3a2c2ff70717caeed1fc6f840b13303cab09029f
Yavuz Tas <yavuzslmtas@gmail.com>
2020-03-12 05:43:34 +0100

    simplify docker configuration

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mvn clean install
 docker container rm spring-boot-webapp
-docker build --build-arg USER=webapp -t boilerplates/spring-boot-webapp-0.0.1 .
+docker build -t boilerplates/spring-boot-webapp-0.0.1 .
 docker run -p 80:8000 --name spring-boot-webapp -e JAVA_OPTS=-Dserver.port=8000 boilerplates/spring-boot-webapp-0.0.1
